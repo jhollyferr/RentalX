ca88c1081baa1cfe3b95ddd79cf22df521874ff3
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-08-08 08:33:51 -0300

    :tada: initial setup

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,28 @@
+import { Request, Response, Router } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoryRepository";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request: Request, response: Response) => {
+  const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists)
+    return response.status(400).json({
+      error: "Category already exists",
+    });
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request: Request, response: Response) => {
+  const all = categoriesRepository.list();
+  return response.json(all);
+});
+
+export { categoriesRoutes };
